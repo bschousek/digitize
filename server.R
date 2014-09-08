c2d01806dcf8504e3917b2928d4beb225716d3dd
bschousek <brian@schousek.com>
2014-09-08 09:45:37 -0500

    working

--- server.R
+++ server.R
@@ -1,116 +1,139 @@
 library(shiny)
-library(readbitmap)
+if (!require(readbitmap)) {
+  ## added to get this to run on the shiny server at work
+  local({r <- getOption("repos")
+         r["CRAN"] <- "http://cran.r-project.org" 
+         options(repos=r)
+  })
+  install.packages("readbitmap")
+  require(readbitmap)
+}
 library(plyr)
 library(ggplot2)
 
-# Define server logic required to plot various variables against mpg
 shinyServer(function(input, output,session) {
-  # image taken from http://rsb.info.nih.gov/ij/plugins/example-plot.html  
+
   output$inplot<-renderImage({
     fname=input$fname
-    if (is.null(fname))
-      list(src=NULL,alt="Please select a file")
-    #fname="c:/temp/images.jpg"
+    if (is.null(fname)) {return(list(src="test",alt="Please select a file"))}
     list(src=fname$datapath,alt=fname)
-    },deleteFile=FALSE)
+  },deleteFile=FALSE)
+  
+  output$outplot<-renderPlot({
+    a<-plotpoints()
+    if (is.null(a))
+      return(NULL)
+    a
+  })
+  
+  
+  output$saveresults=downloadHandler(
+    filename = function() { paste(input$fname, '.rda', sep='')}, 
+    content = function(file) {
+      pointlist=cutcolors()
+      colchoice=as.numeric(input$colors)
+      used_colors<-levels(pointlist$cluster)[colchoice]
+      data=pointlist[pointlist$cluster %in% used_colors,]
+      data$cluster<-factor(data$cluster)
+      save(data, file=file,compress='gzip')
+  })
+  
   
 
-    
+  #load image and convert to data frame with x,y points and colors
   getpointlist<-reactive({
     rgblist=c("r","g","b")
     fname=input$fname
-    if (is.null(fname))
-      return(NULL)
+    if (is.null(fname)) {return(NULL)}
+    
     imsource=read.bitmap(fname$datapath)
     imheight=dim(imsource)[1]
     imwidth=dim(imsource)[2]
     rgbt=dim(imsource)[3]
     
-    
     #first split out the color vectors
-    pointlist=imsource
     #convert the mXnX3 array into an m*nX3 array
-    dim(pointlist)<-c(imheight*imwidth,rgbt)
+    dim(imsource)<-c(imheight*imwidth,rgbt)
     #convert to a data frame and add names for the rgb values
-    pointlist<-as.data.frame(pointlist)
+    imsource<-as.data.frame(imsource)
     #check if the rgb vector has a transparency element as well
-    if (rgbt==3) names(pointlist)<-rgblist else
-      names(pointlist)<-c("r","g","b","t")
-    
+    if (rgbt==3) names(imsource)<-rgblist else
+      names(imsource)<-c("r","g","b","t")
     
     #add x and y values, y changes most rapidly
-    pointlist$y=-seq(1:imheight)
-    pointlist$x=rep(seq(1:imwidth),each=imheight)
-    pointlist[,rgblist]=round(pointlist[,rgblist]*255)
+    imsource$y=-seq(1:imheight)
+    imsource$x=rep(seq(1:imwidth),each=imheight)
+    #bring the rgb values up to 0-255 instead of 0-1
+    imsource[,rgblist]=round(imsource[,rgblist]*255)
+    
     #now create a column with the composite rgb value
     #there are slow ways to do this and faster ways to do this
-    
-    #pointlist$rgb=apply(pointlist[,c("r","g","b")],1,merge_rgb) #5 sec
-    #pointlist$rgb=apply(pointlist[,c("r","g","b")],1,sprintf,fmt="#%02x") #1 second
-    rgbdecimal=apply(pointlist[,rgblist],1,crossprod,c(256*256,256,1)) #fastest
-    pointlist$rgb=rgbdecimal
-#     pointlist$rgb<-lapply(rgbdecimal,sprintf,fmt="%06x")
-#     pointlist$rgb<-as.factor(pointlist$rgb)
-    return(pointlist)
+    #imsource$rgb=apply(imsource[,c("r","g","b")],1,merge_rgb) #5 sec
+    #imsource$rgb=apply(imsource[,c("r","g","b")],1,sprintf,fmt="#%02x") #1 second
+    rgbdecimal=apply(imsource[,rgblist],1,crossprod,c(256*256,256,1)) #fastest
+    imsource$rgb=rgbdecimal
+    return(imsource)
   })
-chunkcolors<-reactive({
-  rgblist=c("r","g","b")
-  pointlist=getpointlist()
-  if (is.null(pointlist))
-    return(NULL)
-#     color_prevalence=sort(table(pointlist$rgb),decreasing=T)
-#     color_cutoff=10000000
-#     if (length(color_prevalence)>color_cutoff) {
-#       colors_to_use=names(color_prevalence[1:color_cutoff])
-#       newpointlist<-pointlist[pointlist$rgb %in% colors_to_use,]
-#       pointlist=newpointlist
-#     }
-    numcolors=input$clusters
-    unique_colors=unique(pointlist[,rgblist])
-    unique_colors$rgb=apply(unique_colors[,rgblist],1,crossprod,c(256*256,256,1))
+  
+  #perform hierarchical cluster analysis on the imported data
+  chunkcolors<-reactive({
+    rgblist=c("r","g","b")
+    pointlist=getpointlist()
+    if (is.null(pointlist)) {return(NULL)}
 
+    unique_colors=pointlist[match(unique(pointlist$rgb),pointlist$rgb),]
     color_cluster=hclust(dist(unique_colors[rgblist],method="manhattan"))
+    return(color_cluster)
+  })
+  #slice the data at chosen number of clusters
+  cutcolors<-reactive({
+    rgblist=c("r","g","b")
+    numcolors=input$clusters
+    pointlist<-getpointlist()
+    color_cluster<-chunkcolors()
+    
+    if (is.null(color_cluster)) {return(NULL)}
+    if (is.null(pointlist)) {return(NULL)}
+    unique_colors=pointlist[match(unique(pointlist$rgb),pointlist$rgb),]
     unique_colors$cluster=cutree(color_cluster,numcolors)
-    
+      
+    centers=aggregate(unique_colors[,rgblist],by=list(unique_colors$cluster),FUN=mean)
+    centers[,rgblist]=floor(centers[,rgblist]+.5)
+    rgbdecimal=apply(centers[,rgblist],1,crossprod,c(256*256,256,1)) #fastest
+    centers$rgb=rgbdecimal
+    centers$rgb<-as.hexmode(centers$rgb)
     #used_colors=as.data.frame(unique_colors$rgb[match(seq(numcolors),unique_colors$cluster)])
-    used_colors=as.data.frame(unique_colors[match(seq(numcolors),unique_colors$cluster),])
-#     color_kmean=kmeans(unique_colors,numcolors)
-#     unique_colors$cluster=color_kmean$cluster
-#      
-#     used_colors=as.data.frame(floor(color_kmean$centers+0.5))
-#     
-    palette=(sprintf("%06x",used_colors$rgb))
-    pointlist$cluster=as.factor(unique_colors$cluster[match(pointlist$rgb,unique_colors$rgb)])
-    pointlist$cluster<-factor(pointlist$cluster,levels=used_colors$cluster,labels=used_colors$rgb)
-    levels(pointlist$cluster)<-as.hexmode(as.numeric(levels(pointlist$cluster)))
-#pointlist$rgb<-as.factor(pointlist$rgb)#,levels=used_colors$cluster,labels=palette)
-
-#b=factor(pointlist$cluster,levels=used_colors$cluster,labels=used_colors$rgb)
-
-
-    #listcolors=apply(colors,1,merge_rgb)
-    cb_options=seq(1:numcolors)
-    names(cb_options)<-palette
-    
-    updateCheckboxGroupInput(session=session,"colors",choices=cb_options,selected=sprintf("%d",cb_options[2:numcolors]),inline=T)
+    #  used_colors=as.data.frame(unique_colors[match(seq(numcolors),unique_colors$cluster),])
+  #     color_kmean=kmeans(unique_colors,numcolors)
+  #     unique_colors$cluster=color_kmean$cluster
+  #      
+  #     used_colors=as.data.frame(floor(color_kmean$centers+0.5))
+  #     
+      palette=(sprintf("%06x",centers$rgb))
+      pointlist$cluster=as.factor(unique_colors$cluster[match(pointlist$rgb,unique_colors$rgb)])
+      pointlist$cluster<-factor(pointlist$cluster,levels=centers$Group.1,labels=centers$rgb)
+      #levels(pointlist$cluster)<-as.hexmode(as.numeric(levels(pointlist$cluster)))
+  #pointlist$rgb<-as.factor(pointlist$rgb)#,levels=used_colors$cluster,labels=palette)
+  
+  #b=factor(pointlist$cluster,levels=used_colors$cluster,labels=used_colors$rgb)
+  
+  
+      #listcolors=apply(colors,1,merge_rgb)
+      cb_options=seq(1:numcolors)
+      names(cb_options)<-palette
+      
+      updateCheckboxGroupInput(session=session,"colors",choices=cb_options,selected=sprintf("%d",cb_options[2:numcolors]),inline=T)
     #numcolors
     
 return(pointlist)
 })
 plotpoints<-reactive({
-  pointlist<-chunkcolors()
+  pointlist<-cutcolors()
   if (is.null(pointlist))
     return (NULL)
   colchoice=as.numeric(input$colors)
   used_colors<-levels(pointlist$cluster)[colchoice]
-  #names(used_colors)<-c("rgb")
   palette=(sprintf("#%s",used_colors))
-#  usedco=(sprintf("%06x",used_colors))
-#   downsample=20000
-#   newpoint_index=sample(seq(nrow(pointlist)),downsample)
-#   #newpointlist=pointlist[newpoint_index,]
-#   newpointlist=pointlist
-#   levels(pointlist$cluster)<-as.hexmode(as.numeric(levels(pointlist$cluster)))
   p<-ggplot(pointlist[pointlist$cluster %in% used_colors,],aes(x,y,color=cluster),size=.01)+geom_point()+
     facet_wrap( ~ cluster)+
     scale_color_manual(values=palette)+
@@ -119,28 +142,5 @@ plotpoints<-reactive({
   return(p)
   
 })
-  output$outplot<-renderPlot({
-  a<-plotpoints()
-  if (is.null(a))
-    return(NULL)
-  a
-    })
-output$noise=renderText({
-  
-  outputOptions(output,"inplot")
-})
-output$saveresults=downloadHandler(
-  filename = function() { paste(input$fname, '.rda', sep='')}, 
-  content = function(file) {
-    pointlist=chunkcolors()
-    colchoice=as.numeric(input$colors)
-    used_colors<-levels(pointlist$cluster)[colchoice]
-    #names(used_colors)<-c("rgb")
-    
-    data=pointlist[pointlist$cluster %in% used_colors,]
-    save(data, file=file,compress='gzip')
-    })
-  
-
 
 })
--- ui.R
+++ ui.R
@@ -7,18 +7,39 @@ shinyUI(pageWithSidebar(
   headerPanel("Plot digitizer"),
   
   sidebarPanel(
-    fileInput("fname",label=h4("Plot Input"),accept = c("image/bmp","image/jpeg","image/png")),
-    #actionButton("processFile",label="Process File"),
+    fileInput("fname",label=h4("Choose Plot File"),accept = c("image/bmp","image/jpeg","image/png")),
+    hr(),
+    h4("Source Plot"),
     imageOutput("inplot",height="100%"),
-    sliderInput("clusters","Number of Colors to classify",2,16,6),
-    checkboxGroupInput("colors","color list",choices=c("blue","caffo","leek","peng","cyan"),inline=T),
-    verbatimTextOutput("noise"),
+    hr(),
+    sliderInput("clusters",label=h4("Number of Colors to classify"),2,16,6),
+    hr(),
+    checkboxGroupInput("colors",h4("Colors to Display"),choices=c("blue","caffo","leek","peng","cyan"),inline=T),
+    hr(),
+    h4("Save displayed plots as R data file"),
     downloadButton("saveresults")
   ),
   
   mainPanel(
-    
-    plotOutput("outplot")
+    plotOutput("outplot"),
+    hr(),
+    h2("Instructions"),
+    HTML("This app aids in reproducing collected data from plots which have been saved as images.<br>
+    1) Upload a file using the  'choose file' button. The image will show up in the left column. 
+        The image can be uploaded from the local file system or from a URL.
+        For an example, try the following file from the Python sympy documentation, found via a Google search for 'plot':<br>"),
+
+    h5("http://docs.sympy.org/dev/_images/plot.png"),
+    HTML("
+    2) The right column will show the results of breaking the input image into its constituent colors
+        as identified by the hclust function for hierarchical cluster analysis.<br>
+    3) The level at which the cluster map is cut can be adjusted using the slider. Adjust the slider until
+         the displayed plots include isolated versions of the data of interest.<br>
+    4) The colors displayed can be turned on and off with the checkboxes under 'Colors to Display'.
+         By default the most common color in the image (assumed to be the background color) is not displayed.
+    4) Clicking the 'Download' button will save the data represented by the displayed plots as an R .rda file.
+        Depending on the OS and browser used, this file will most likely end up in a 'Downloads' folder.
+         ")
     
   )
 ))
